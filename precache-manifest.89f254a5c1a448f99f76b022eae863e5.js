20a1d49242a31c9c23867b34870a57c5207d77cc
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2019-01-02 20:05:37 +0100

    Updates

---
+++ precache-manifest.89f254a5c1a448f99f76b022eae863e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf28236b852d33b93f18",
+    "url": "/static/js/main.cf28236b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "cf28236b852d33b93f18",
+    "url": "/static/css/main.120987b7.chunk.css"
+  },
+  {
+    "revision": "94e86aa149ffddb8cde3e88e2bac86c7",
+    "url": "/index.html"
+  }
+];
